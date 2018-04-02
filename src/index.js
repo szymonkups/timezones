1a8070b75ff3920910b91245105d9075247030eb
Szymon Kupś <szymonkups@gmail.com>
2018-04-02 22:13:58 +0200

    Added moment library.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import App from './components/app/App';
+import App from './containers/app/App';
 import rootReducer from './reducers';
 
 // Create redux store starting from root reducer.
