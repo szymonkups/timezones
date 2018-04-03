c6835b88e610c97c6e1092c170c7d5b4593e7a04
Szymon Kupś <szymonkups@gmail.com>
2018-04-03 17:59:32 +0200

    Updated some list styles.

--- src/components/Timezone/Timezone.js
+++ src/components/Timezone/Timezone.js
@@ -4,16 +4,18 @@ import styles from './Timezone.css';
 
 export default props => (
 	<li className="row ">
-		<div className={ 'row col-md-6 col-md-offset-3 ' + styles.timezone }>
-			<div className={ 'col-sm-6 ' + styles.name } >
-				{ props.name }
+		<div className={ 'col-md-6 col-md-offset-3 ' + styles.timezone }>
+			<div className="row">
+				<div className={ 'col-sm-6 ' + styles.name } >
+					{ props.name }
+				</div>
+				<div className={ 'col-sm-4 col-xs-10 ' + styles.time }>
+					{ getTimeInZone( props.time, props.name ) }
+				</div>
+				<a className={ 'col-sm-2 col-xs-2 ' + styles.button }>
+					<i className="fas fa-trash-alt"></i>
+				</a>
 			</div>
-			<div className={ 'col-sm-4 ' + styles.time }>
-				{ getTimeInZone( props.time, props.name ) }
-			</div>
-			<a className={ 'col-sm-2 ' + styles.button }>
-				<i className="fas fa-trash-alt"></i>
-			</a>
 		</div>
 	</li>
 );
