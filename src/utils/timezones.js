c04fc24007021116969fd4cd8861e6d949a0fc61
Szymon Kupś <szymonkups@gmail.com>
2018-04-05 21:21:24 +0200

    Added comments to timezone utilities.

--- src/utils/timezones.js
+++ src/utils/timezones.js
@@ -1,6 +1,5 @@
 /**
  * This file contains timezone utilities build on top of moment-timezone library.
- * Moment is not used directly so it might be easier to exchange it in the future.
  */
 
 import moment from 'moment-timezone';
@@ -23,14 +22,35 @@ export function getCurrentTime() {
 	return moment().format();
 }
 
+/**
+ * Returns time in given timezone.
+ *
+ * @param {String} time Time string in any timezone.
+ * @param {String} zone Name of the destination timezone.
+ * @return {Object} Time information in given timezone.
+ */
 export function getTimeInZone( time, zone ) {
 	return moment( time ).tz( zone );
 }
 
+/**
+ * Returns all available timezones.
+ *
+ * @return {Array.<String>}
+ */
 export function getAllTimezones() {
 	return moment.tz.names();
 }
 
+/**
+ * Returns timezone string from given hour, minute and period.
+ *
+ * @param {String} hours
+ * @param {String} minutes
+ * @param {String} period
+ * @param {String{ timezone
+ * @return {String}
+ */
 export function getTimeString( hours, minutes, period, timezone ) {
 	return moment.tz( `${ hours } ${ minutes } ${ period }`, 'hh mm A', timezone ).format();
 }
